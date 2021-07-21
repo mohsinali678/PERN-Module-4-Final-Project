8b41550a18678dc9ae47213146943e8453db2a49
Mohsin Tahir <mohsintahir@Mohsins-MBP.home>
2021-07-20 20:08:29 -0400

    Updated dbConfig.js to be deployed on Heroku

--- back-end/db/dbConfig.js
+++ back-end/db/dbConfig.js
@@ -1,13 +1,38 @@
+//DEPENDECIES
 const pgp = require("pg-promise")();
+
+
+//CONFIGURATION
 require("dotenv").config();
+const { 
+  DATABASE_URL, 
+  PG_HOST, 
+  PG_PORT, 
+  PG_DATABASE, 
+  PG_USER, 
+  PG_PASSWORD } = process.env;
 
-const cn = {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER,
-};
 
+//Connection Object
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER
+    };
+
+
+//Creating Instance of PG_Promise And Passing CO to PG_Promise
 const db = pgp(cn);
 
+
+//EXPORTS
 module.exports = db;
