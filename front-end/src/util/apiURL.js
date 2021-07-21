3ad9e94c2c117b6dbb219373cc6a52318c9cd303
Mohsin Tahir <mohsintahir@Mohsins-MBP.home>
2021-07-21 12:58:28 -0400

    Updated old URL with Heroku URL

--- front-end/src/util/apiURL.js
+++ front-end/src/util/apiURL.js
@@ -2,6 +2,6 @@ export default function apiURL() {
     return (
         window.location.hostname === "localhost" || "127.0.0.1"
         ? "http://localhost:3003"
-        : "http://127.0.0.1:3003"
+        : "https://sports-gear-api.herokuapp.com"
     )
 }
