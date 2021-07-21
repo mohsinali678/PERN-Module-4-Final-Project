55dc4464016d11cecb37ab66c131cd2846e8fec0
Jamee Hengari <jhengari@hotmail.com>
2021-07-21 14:06:43 -0400

    removed comments

--- back-end/db/dbConfig.js
+++ back-end/db/dbConfig.js
@@ -8,13 +8,6 @@ const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
 //Connection Object
 const cn = DATABASE_URL
   ? {
-<<<<<<< HEAD
-      // host: PG_HOST,
-      // port: PG_PORT,
-      // database: PG_DATABASE,
-      // user: PG_USER
-=======
->>>>>>> 3ad9e94c2c117b6dbb219373cc6a52318c9cd303
       connectionString: DATABASE_URL,
       max: 30,
       ssl: {
