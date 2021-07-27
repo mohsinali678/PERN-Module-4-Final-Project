6e79a0fef178138b47a9e8971cfc0e2132e036ed
Mohsin Tahir <mohsintahir@Mohsins-MBP.home>
2021-07-26 23:29:17 -0400

    Added Image Upload

--- front-end/src/Components/ProductEditForm.js
+++ front-end/src/Components/ProductEditForm.js
@@ -4,124 +4,139 @@ import axios from "axios";
 import apiURL from "../util/apiURL.js";
 
 export default function ProductEditForm() {
-  //Initializing Variables
-  const API = apiURL();
-  const { id } = useParams();
-  const [product, setProduct] = useState({
-    name: "",
-    image_url: "",
-    price: 0,
-    number_of_items: 0,
-    in_stock: false,
-  });
-  const history = useHistory();
+    //Initializing Variables
+    const API = apiURL();
+    const { id } = useParams();
+    const [product, setProduct] = useState({
+        name : "",
+        image_url : "",
+        file : null,
+        price : 0,
+        number_of_items: 0,
+        description_of_item:" ",
+        in_stock : false
+    });
+    const history = useHistory();
 
-  //Update State Upon Page Load
-  useEffect(() => {
-    axios
-      .get(`${API}/products/${id}`)
-      .then(
-        (response) => setProduct(response.data.payload),
-        (error) => {
-          console.error(`Error: ${error}`);
-          history.push(`/not-found`);
-        }
-      )
-      .catch((c) => console.warn(`Warning: ${c}`));
-  }, [id, history, API]);
 
-  //Update product Function
-  const updateProduct = (product) => {
-    axios
-      .put(`${API}/products/${id}`, product)
-      .then(
-        () => {
-          history.push("/products");
-        },
-        (error) => {
-          console.error(`Error: ${error}`);
-        }
-      )
-      .catch((c) => console.warn(`Warning: ${c}`));
-  };
+    //Update State Upon Page Load
+    useEffect(() => {
+        axios.get(`${API}/products/${id}`)
+        .then(
+            response => {
+                setProduct(response.data.payload);
+            }
+        ,
+            (error) => {
+            console.error(`Error: ${error}`)
+            history.push(`/not-found`)
+            }
+        )
+        .catch(c => console.warn(`Warning: ${c}`));
+    }, [id, history, API])
 
-  //Handle Text Change
-  const handleTextChange = (event) => {
-    setProduct({ ...product, [event.target.id]: event.target.value });
-  };
 
-  //Handle Submit
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    updateProduct(product);
-  };
+    //Update product Function
+    const updateProduct = (product) => {
+        axios.put(`${API}/products/${id}`, product)
+        .then(
+            () => {
+                history.push("/products");
+            }
+            ,
+            (error) => {
+                console.error(`Error: ${error}`)
+            }
+        )
+        .catch(c => console.warn(`Warning: ${c}`));
+    }
 
-  return (
-    <div className="Edit">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          placeholder="Name of Product"
-          value={product.name}
-          onChange={handleTextChange}
-          required
-        />
-        <br />
-        <label htmlFor="image_url">Image URL:</label>
-        <input
-          id="image_url"
-          name="image_url"
-          type="text"
-          placeholder="http[s]://"
-          value={product.image_url}
-          onChange={handleTextChange}
-          pattern="http[s]*://.+"
-          required
-        />
-        <br />
-        <label htmlFor="price">Price:</label>
-        <input
-          id="price"
-          name="price"
-          type="number"
-          placeholder="Price $0.00"
-          value={product.price}
-          onChange={handleTextChange}
-          required
-        />
-        <br />
-        <label htmlFor="number_of_items">Total Number of Items:</label>
-        <input
-          id="number_of_items"
-          name="number_of_items"
-          type="number"
-          placeholder="Total Number of items available for sale"
-          value={product.number_of_items}
-          onChange={handleTextChange}
-          required
-        />
-        <br />
 
-        <label htmlFor="description_of_item">Description:</label>
-        <textarea
-          id="description_of_item"
-          name="description_of_item"
-          placeholder="Describe the item....."
-          value={product.description_of_item}
-          onChange={handleTextChange}
-          required
-        />
-        <br />
 
-        <input id="submit" type="submit" />
-      </form>
-      <br />
-      <Link id="neverMind" to={`/products/${id}`}>
-        <button>Never mind!</button>
-      </Link>
-    </div>
-  );
+    //Handle Text Change
+    const handleTextChange = (event) => {
+        setProduct({...product, [event.target.id] : event.target.value});
+    }
+    
+
+    //File Select Handler
+    const fileSelectHandler = (event) => {
+        product[event.target.id] = event.target.files[0]; 
+        product['image_url'] = URL.createObjectURL(product[event.target.id])
+    }
+    
+    
+    
+    //Handle Submit
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        updateProduct(product);
+    }
+
+
+    return (
+        <div className="Edit">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name:</label>
+                <input 
+                    id="name"
+                    name="name"
+                    type="text"
+                    placeholder="Name of Product"
+                    value={product.name}
+                    onChange={handleTextChange}
+                    required
+                />
+                <br />
+                <label htmlFor="file">Image URL:</label>
+                <input
+                    id="file"
+                    name="file"
+                    type="file"
+                    onChange={fileSelectHandler}
+                    required
+                />
+                <br />
+                <label htmlFor="price">Price:</label>
+                <input
+                    id="price"
+                    name="price"
+                    type="number"
+                    placeholder="Price $0.00"
+                    value={product.price}
+                    onChange={handleTextChange}
+                    required
+                />
+                <br />
+                <label htmlFor="number_of_items">Total Number of Items:</label>
+                <input
+                    id="number_of_items"
+                    name="number_of_items"
+                    type="number"
+                    placeholder="Total Number of items available for sale"
+                    value={product.number_of_items}
+                    onChange={handleTextChange}
+                    required
+                />
+                <br />
+
+                <label htmlFor="description_of_item">Description:</label>
+                <textarea
+                    id="description_of_item"
+                    name="description_of_item"
+                    placeholder="Describe the item....."
+                    value={product.description_of_item}
+                    onChange={handleTextChange}
+                    required
+                />
+                <br />
+
+                <input type="submit" />
+            </form>
+
+            <Link to={`/products/${id}`}>
+                <button>Never mind!</button>
+            </Link>
+        </div>
+    )
 }
--- front-end/src/Components/ProductNewForm.js
+++ front-end/src/Components/ProductNewForm.js
@@ -1,109 +1,125 @@
 import { useState } from "react";
 import { useHistory, withRouter } from "react-router-dom";
 import axios from "axios";
-import apiURL from "../util/apiURL.js";
+import apiURL from "../util/apiURL.js"
 
 export default withRouter(function ProductNewForm() {
-  //INITIALIZING VARIABLES
-  const API = apiURL();
-  const [product, setProduct] = useState({
-    name: "",
-    image_url: "",
-    price: 0,
-    number_of_items: 0,
-    description_of_item: "",
-    in_stock: false,
-  });
-  const history = useHistory();
+    //INITIALIZING VARIABLES
+    const API = apiURL();
+    const [product, setProduct] = useState({
+        name : "",
+        image_url : "",
+        file : null,
+        price : 0,
+        number_of_items: 0,
+        description_of_item:"",
+        in_stock : false
+    });
+    // const [file, setFile] = useState(null);
+    const history = useHistory();
 
-  // Add Product Function
-  const addProduct = (newProduct) => {
-    axios
-      .post(`${API}/products`, newProduct)
-      .then(
-        () => {
-          history.push("/products");
-        },
-        (error) => {
-          console.error(`Error: ${error}`);
-        }
-      )
-      .catch((c) => console.warn(`Warning: ${c}`));
-  };
 
-  //Handle Text Change
-  const handleTextChange = (event) => {
-    setProduct({ ...product, [event.target.id]: event.target.value });
-  };
+    // Add Product Function
+    const addProduct = (newProduct) => {
+        axios.post(`${API}/products`, newProduct)
+            .then(
+                (response) => {
+                    history.push("/products");
+                }
+                ,
+                (error) => {
+                    console.error(`Error: ${error}`)
+                }
+            )
+            .catch(c => console.warn(`Warning: ${c}`));
+        
+        
+            
+    }
 
-  //Handle Submit
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    addProduct(product);
-  };
 
-  return (
-    <div className="New">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          placeholder="Name of Product"
-          value={product.name}
-          onChange={handleTextChange}
-          required
-        />
-        <br />
-        <label htmlFor="image_url">Image URL:</label>
-        <input
-          id="image_url"
-          name="image_url"
-          type="text"
-          placeholder="http[s]://"
-          value={product.image_url}
-          onChange={handleTextChange}
-          pattern="http[s]*://.+"
-          required
-        />
-        <br />
-        <label htmlFor="price">Price:</label>
-        <input
-          id="price"
-          name="price"
-          type="number"
-          placeholder="Price $0.00"
-          value={product.price}
-          onChange={handleTextChange}
-          required
-        />
-        <br />
-        <label htmlFor="number_of_items">Total Number of Items:</label>
-        <input
-          id="number_of_items"
-          name="number_of_items"
-          type="number"
-          placeholder="Total Number of items available for sale"
-          value={product.number_of_items}
-          onChange={handleTextChange}
-          required
-        />
-        <br />
+    //Handle Text Change
+    const handleTextChange = (event) => {
+        setProduct({...product, [event.target.id] : event.target.value});
+    }
 
-        <label htmlFor="description_of_item">Description:</label>
-        <textarea
-          id="description_of_item"
-          name="description_of_item"
-          placeholder="Describe the item....."
-          value={product.description_of_item}
-          onChange={handleTextChange}
-          required
-        />
-        <br />
 
-        <input id="submit" type="submit" />
-      </form>
-    </div>
-  );
+    //File Select Handler
+    const fileSelectHandler = (event) => {
+        product[event.target.id] = event.target.files[0]; 
+        product['image_url'] = URL.createObjectURL(product[event.target.id])
+    }
+
+
+    //Handle Submit
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        addProduct(product);
+    }
+    
+
+    return (
+        <div className="New">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name:</label>
+                <input 
+                    id="name"
+                    name="name"
+                    type="text"
+                    placeholder="Name of Product"
+                    value={product.name}
+                    onChange={handleTextChange}
+                    required
+                />
+                <br />
+
+                <label htmlFor="file">Image URL:</label>
+                <input
+                    id="file"
+                    name="file"
+                    type="file"
+                    onChange={fileSelectHandler}
+                    required
+                />
+                <br />
+                
+                <label htmlFor="price">Price:</label>
+                <input
+                    id="price"
+                    name="price"
+                    type="number"
+                    placeholder="Price $0.00"
+                    value={product.price}
+                    onChange={handleTextChange}
+                    required
+                />
+                <br />
+
+                <label htmlFor="number_of_items">Total Number of Items:</label>
+                <input
+                    id="number_of_items"
+                    name="number_of_items"
+                    type="number"
+                    placeholder="Total Number of items available for sale"
+                    value={product.number_of_items}
+                    onChange={handleTextChange}
+                    required
+                />
+                <br /> 
+
+                <label htmlFor="description_of_item">Description:</label>
+                <textarea
+                    id="description_of_item"
+                    name="description_of_item"
+                    placeholder="Describe the item....."
+                    value={product.description_of_item}
+                    onChange={handleTextChange}
+                    required
+                />
+                <br /> 
+
+                <input type="submit" />
+            </form>
+        </div>
+    )
 });
